13ae7dd131e7efd229a52b197770d07e3a0ba9d3
cat <macy.cordner@gmail.com>
2023-06-26 11:25:30 -0700

    added the arrays to pick the words from

---
+++ scripts/script.js
@@ -0,0 +1,111 @@
+"use strict"
+// Generation 1 (Kanto)
+const gen1Pokemon = [
+    "Bulbasaur",
+    "Charmander",
+    "Squirtle",
+    "Pikachu",
+    "Jigglypuff",
+    "Meowth",
+    "Psyduck",
+    "Growlithe",
+    "Poliwag",
+    "Abra"
+  ];
+  
+  // Generation 2 (Johto)
+  const gen2Pokemon = [
+    "Chikorita",
+    "Cyndaquil",
+    "Totodile",
+    "Togepi",
+    "Marill",
+    "Wooper",
+    "Hoothoot",
+    "Phanpy",
+    "Larvitar",
+    "Sneasel"
+  ];
+  
+  // Generation 3 (Hoenn)
+  const gen3Pokemon = [
+    "Treecko",
+    "Torchic",
+    "Mudkip",
+    "Ralts",
+    "Zigzagoon",
+    "Seedot",
+    "Wurmple",
+    "Shroomish",
+    "Tailow",
+    "Aron"
+  ];
+  
+  // Generation 4 (Sinnoh)
+  const gen4Pokemon = [
+    "Turtwig",
+    "Chimchar",
+    "Piplup",
+    "Bidoof",
+    "Starly",
+    "Shinx",
+    "Kricketot",
+    "Cranidos",
+    "Riolu",
+    "Gible"
+  ];
+  
+  // Generation 5 (Unova)
+  const gen5Pokemon = [
+    "Snivy",
+    "Tepig",
+    "Oshawott",
+    "Patrat",
+    "Lillipup",
+    "Pidove",
+    "Blitzle",
+    "Drilbur",
+    "Tympole",
+    "Deino"
+  ];
+  
+  // Generation 6 (Kalos)
+  const gen6Pokemon = [
+    "Chespin",
+    "Fennekin",
+    "Froakie",
+    "Bunnelby",
+    "Fletchling",
+    "Inkay",
+    "Spritzee",
+    "Swirlix",
+    "Clauncher",
+    "Tyrunt"
+  ];
+  
+  // Generation 7 (Alola)
+  const gen7Pokemon = [
+    "Rowlet",
+    "Litten",
+    "Popplio",
+    "Pikipek",
+    "Yungoos",
+    "Grubbin",
+    "Rockruff",
+    "Stufful",
+    "Bounsweet",
+    "Jangmo-o"
+  ];
+  // Generation 8 (Galar)
+const gen8Pokemon = [
+    "Grookey",
+    "Scorbunny",
+    "Sobble",
+    "Rookidee",
+    "Blipbug",
+    "Nickit",
+    "Gossifleur",
+    "Wooloo",
+    "Chewtle",
+    "Yamper"
+  ];
